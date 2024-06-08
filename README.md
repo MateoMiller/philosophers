c53d33620ce1489b4abc109c1fd1de4ab268fb96
yurii <yurii@DESKTOP-M79MGU7>
2024-06-09 01:59:47 +0500

    Потокобезопасная версия

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # philosophers
 Задача с курса по компьютерным наукам
+
+2 философа
+Philosopher: 0
+Min time: 209
+Max time: 319
+Average time: 219.5
+Philosopher: 1
+Min time: 209
+Max time: 247
+Average time: 219.068
+
+10 философов
+Philosopher: 0
+Min time: 212
+Max time: 435
+Average time: 275.328
+Philosopher: 1
+Min time: 211
+Max time: 385
+Average time: 274.808
+Philosopher: 2
+Min time: 210
+Max time: 380
+Average time: 274.388
+Philosopher: 3
+Min time: 211
+Max time: 383
+Average time: 274.78
+Philosopher: 4
+Min time: 211
+Max time: 385
+Average time: 274.388
+Philosopher: 5
+Min time: 210
+Max time: 386
+Average time: 274.872
+Philosopher: 6
+Min time: 212
+Max time: 385
+Average time: 274.456
+Philosopher: 7
+Min time: 210
+Max time: 437
+Average time: 275.204
+Philosopher: 8
+Min time: 210
+Max time: 383
+Average time: 274.732
+Philosopher: 9
+Min time: 210
+Max time: 385
+Average time: 274.3
+
+100 философов
+У всех примерно
+Philosopher: 56
+Min time: 211
+Max time: 610
+Average time: 286.34
+Philosopher: 57
+Min time: 209
+Max time: 613
+Average time: 285.892
+Philosopher: 58
+Min time: 209
+Max time: 624
+Average time: 285.412
+Philosopher: 59
+Min time: 209
+Max time: 604
+Average time: 285.868
--- main.cpp
+++ main.cpp
@@ -3,44 +3,76 @@
 #include <mutex>
 #include <vector>
 #include <chrono>
+#include <algorithm>
+#include <numeric>
 
-const int number_of_Philosophers = 5;
-const int eatingDuration = 1000;
-const int thinkingDuration = 1000;
-const int iterationsLimit = 15;
+const int number_of_Philosophers = 10;
+const int eatingDuration = 100;
+const int thinkingDuration = 100;
+const int iterationsLimit = 250;
+
+enum PhilosopherState{
+    Waiting,
+    Eating
+};
 
 std::vector<std::mutex> forks(number_of_Philosophers);
 std::vector<std::thread> philosophers(number_of_Philosophers);
-std::vector<int> eatingTime(number_of_Philosophers);
-std::vector<int> thinkingTime(number_of_Philosophers);
+std::vector<PhilosopherState> philosophersStates(number_of_Philosophers);
+std::vector<std::vector<double>> elapsedForPhilosopher(number_of_Philosophers);
+std::mutex waiter;
 
+void WaitForMyOrder(int id, int leftForkId, int rightForkId){
+
+    auto& state = philosophersStates.at(id);
+    auto& left = philosophersStates.at(leftForkId);
+    auto& right = philosophersStates.at(rightForkId);
+    while(state != Eating){
+        waiter.lock();
+        if (left == Waiting || right == Waiting) {
+            state = Eating;
+        }
+        waiter.unlock();
+    }
+}
 
 void philosopher(int id)
 {
+    auto& MyState = philosophersStates.at(id);
     int leftFork = id;
     int rightFork = (id + 1) % number_of_Philosophers;
 
-    int i = iterationsLimit;
+    int i = 0;
+    std::vector<double> iterationTime(iterationsLimit);
 
-    while (i > 0)
+    while (i < iterationsLimit)
     {
-        std::cout << "Philosopher " << id << " is thinking." << std::endl;
+        auto startTime = std::chrono::steady_clock::now();
+        std::cout << "P " << id << " th" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(thinkingDuration));
 
+
+        WaitForMyOrder(id, leftFork, rightFork);
         forks[leftFork].lock();
         forks[rightFork].lock();
 
-        std::cout << "Philosopher " << id << " started eating." << std::endl;
+        std::cout << "P " << id << " eat" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(eatingDuration));
 
         forks[leftFork].unlock();
         forks[rightFork].unlock();
+        MyState = Waiting;
 
-        std::cout << "Philosopher " << id << " finished eating." << std::endl;
 
-        i--;
+        std::cout << "P " << id << " fin" << std::endl;
+
+        auto endTime = std::chrono::steady_clock::now();
+        iterationTime[i] = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+        i++;
     }
+    elapsedForPhilosopher.at(id) = iterationTime;
 }
+
 
 int main()
 {
@@ -54,5 +86,31 @@ int main()
         philosophers[i].join();
     }
 
+    for (int i = 0; i < elapsedForPhilosopher.size(); i++){
+        std::vector<double> elapsed = elapsedForPhilosopher.at(i);
+        double min_value = *std::min_element(elapsed.begin(), elapsed.end());
+
+        double max_value = *std::max_element(elapsed.begin(), elapsed.end());
+
+        double sum = std::accumulate(elapsed.begin(), elapsed.end(), 0.0);
+        double average_value = sum / elapsed.size();
+
+        std::cout << "Philosopher: " << i << std::endl;
+        std::cout << "Min time: " << min_value << std::endl;
+        std::cout << "Max time: " << max_value << std::endl;
+        std::cout << "Average time: " << average_value << std::endl;
+    }
+
     return 0;
+}
+
+double avg(const std::vector<double>& vector)
+{
+    double total = 0;
+    for (double i : vector)
+    {
+        total += i;
+    }
+
+    return total / vector.size();
 }
